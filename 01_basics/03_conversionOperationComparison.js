4b658b8cd4887dfd95be75f58cf2367f434a26de
Bhaskar Paul <bhaskarp986@gmail.com>
2024-05-25 18:05:37 +0530

    added 03_conversionOperationComparison file

---
+++ 01_basics/03_conversionOperationComparison.js
@@ -0,0 +1,45 @@
+let a=10
+console.log(a,typeof a)
+
+let b=String(a)
+console.log(b,typeof b)
+
+let c="10abc"
+let d=Number(c)
+console.log(d,typeof d)
+
+/*OUTPUT
+
+a=> 10 number
+b=> 10 string
+c=> 10abc string
+d=> NaN number
+1=> true boolean
+0=> false boolean
+
+*/
+
+/********************Operations*********************/
+
+/*
+
++ => add + concat
+- => sub
+* => mul
+/ => div
+% => modulus 
+..... etc 
+
+*/
+
+/* 
+
+>  => greater
+<  => lesser 
+>= => greater equal
+<= => lesser equal
+== => equals
+=== => strictly equals ( type also )
+!= => not equals
+
+*/
