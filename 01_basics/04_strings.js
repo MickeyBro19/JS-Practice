b7318b3b348c85594da19f664712d610a74f8261
Bhaskar Paul <bhaskarp986@gmail.com>
2024-05-28 19:07:43 +0530

    added 04_strings file

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,15 @@
+const name= "Mickey"
+const age= 25
+
+console.log(name+"  "+age) //dont use the old way
+console.log(`My name is ${name} and my age is ${age}`) //uses backticks(``) and string interpolation for better string output
+
+/*OUTPUT
+
+Mickey  25                          // old way
+My name is Mickey and my age is 25  // new way
+
+ */
+
+let str=new String('MickeyFC')
+
