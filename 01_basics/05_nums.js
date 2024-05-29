b70edecd25ca1255a414272305d320b00becce5f
Bhaskar Paul <bhaskarp986@gmail.com>
2024-05-29 17:57:23 +0530

    completed 01_basics

--- 01_basics/05_nums.js
+++ 01_basics/05_nums.js
@@ -1,16 +1,16 @@
-const a=100
+const a = 100;
 console.log(a);
 
-const b=new Number(400)
+const b = new Number(400);
 console.log(b);
 
-const balance=123.56789
+const balance = 123.56789;
 
-console.log(balance.toFixed(2));  //defines no. decimal points
-console.log(balance.toPrecision(4));   //rounds off data
+console.log(balance.toFixed(2)); //defines no. decimal points
+console.log(balance.toPrecision(4)); //rounds off data
 
-const hundreds=10000000000
-console.log(hundreds.toLocaleString('en-IN'))   //inserts comma at positions
+const hundreds = 10000000000;
+console.log(hundreds.toLocaleString("en-IN")); //inserts comma at positions
 
 /* OUTPUT 
     100
@@ -20,11 +20,7 @@ console.log(hundreds.toLocaleString('en-IN'))   //inserts comma at positions
     10,00,00,00,000
 */
 
+const min = 10;
+const max = 15;
 
-const min=10
-const max=15
-
-console.log(Math.floor(Math.random()*(max-min + 1)) + min)  //getting a random number between max and min
-
-
-
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); //getting a random number between max and min
---
+++ 01_basics/06_dates_and_time.js
@@ -0,0 +1,20 @@
+const myDate=new Date()
+
+console.log(myDate)
+console.log(typeof myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toJSON())
+
+
+/* OUTPUT
+
+2024-05-28T14:11:46.175Z
+object
+Tue May 28 2024 19:41:46 GMT+0530 (India Standard Time)
+Tue May 28 2024
+5/28/2024, 7:41:46 PM
+2024-05-28T14:11:46.175Z
+
+ */
